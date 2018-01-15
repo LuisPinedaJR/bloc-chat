80c85d5ad17ef70f822752e1db5a7f8c762586ee
Luis Pineda JR <lspnd00@gmail.com>
2018-01-14 20:16:52 -0500

    trouble with showing messages

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,19 +1,31 @@
 (function(){
-        function HomeCtrl(Room, $uibModal){
-                this.rooms = Room.all;
+        function HomeCtrl(Room, $scope, $uibModal, Message){
+              var home = this;
+                home.rooms = Room.all;
+                // $scope.currentRoom = null;
+                home.currentRoom = null;
 
-                this.addRoom = function() {
+                home.setCurrentRoom = function(room){
+                        home.currentRoom = room;
+                        home.messages = Message.getByRoomId(home.currentRoom.$id);
+                        console.log(home.messages)
+
+                };
+
+                home.addRoom = function() {
                         $uibModal.open({
-                              animation: false,
+                              animation: false  ,
                               templateUrl: '/templates/modal.html',
                               controller: 'ModalCtrl as modal',
                               size: 'sm'
 
                       });
-                }
+                };
+
+
         }
 
   angular
        .module('blocChat')
-       .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+       .controller('HomeCtrl', ['Room', 'Message', '$scope', '$uibModal', HomeCtrl]);
 })();
